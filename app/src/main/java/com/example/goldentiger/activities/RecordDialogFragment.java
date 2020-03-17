4f6ee86ea4063821e84e3b00e4fd6e14c90626de
Anil9757 <56797977+Anil9757@users.noreply.github.com>
2020-03-17 19:52:19 +0100

    Update RecordDialogFragment.java

--- app/src/main/java/com/example/goldentiger/activities/RecordDialogFragment.java
+++ app/src/main/java/com/example/goldentiger/activities/RecordDialogFragment.java
@@ -11,10 +11,12 @@ public class RecordDialogFragment extends DialogFragment {
 
     @NonNull
     @Override
+    //Dialogue Fragment affichant le message Ce livre a été rajouté dans les favoris.
+    //Il est activé uniquement quand on clique sur le bouton où il y l'étoile (voir code BookDetails.java).
     public Dialog onCreateDialog (Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        builder.setMessage("Ce livre à été rajouté dans vos favoris");
+        builder.setMessage("Ce livre a été rajouté dans vos favoris");
         return builder.create();
     }
 }
