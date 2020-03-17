c6277719d96356e4d5d16074d354ca6422434b69
Anil9757 <56797977+Anil9757@users.noreply.github.com>
2020-03-17 19:56:55 +0100

    Update RecyclerViewAdapter.java

--- app/src/main/java/com/example/goldentiger/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/goldentiger/adapter/RecyclerViewAdapter.java
@@ -17,9 +17,9 @@ import com.bumptech.glide.request.RequestOptions;
 
 import java.util.*;
 
+import com.example.goldentiger.R;
 import com.example.goldentiger.activities.BookDetails;
 import com.example.goldentiger.model.BookView;
-import com.example.goldentiger.R;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
 
@@ -41,12 +41,17 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         View view;
         LayoutInflater inflater = LayoutInflater.from(nContext);
+        //Dans cette vue il y a les éléments comme l'affichage de l'image, le titre, le prix et le nom de l'auteur formant
+        //une ligne qui apparaitre dans la recyclerview.
         view = inflater.inflate(R.layout.book_row_item, parent, false);
         final MyViewHolder viewHolder = new MyViewHolder(view);
+        //Cette fonction ne se réalise que quand on clique sur sur un livre se trouvant dans la recyclerview.
+        //Il ramène à l'activité BookDetails où l'on voit les détails du livre.
         viewHolder.container.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(nContext , BookDetails.class);
+                //Par l'intermédiaire du viewholder, on récupère chaque élément pour les envoyés dans l'activité BookDetails.
                 int pos = viewHolder.getAdapterPosition();
                 i.putExtra("book_title" ,nData.get(pos).getTitle());
                 i.putExtra("book_author" ,nData.get(pos).getAuthor());
@@ -56,7 +61,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                 i.putExtra("book_info" ,nData.get(pos).getUrl());
                 i.putExtra("book_thumbnail" ,nData.get(pos).getImage_url());
 
-
+                //Lancement de l'activité BookDetails.
                 nContext.startActivity(i);
             }
         });
@@ -67,16 +72,20 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.MyViewHolder holder, int position) {
 
+        //on affecte les composants design aux valeurs récupérés
+        //Les éléments ci-dessous seront affichés dans la recyclerview
         BookView book = nData.get(position);
         holder.tTitle.setText(book.getTitle());
         holder.tAuthor.setText(book.getAuthor());
         holder.tCategory.setText(book.getCategory());
         holder.tPrice.setText(book.getPrice());
 
+        //Utilisation de la bibliothèque Glide pour l'affichage du livre.s
         Glide.with(nContext).load(book.getImage_url()).apply(options).into(holder.vThumbnail);
 
     }
 
+    //La taille de liste de données.
     @Override
     public int getItemCount() {
         return nData.size();
@@ -93,6 +102,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         {
             super(itemView);
 
+            //Composant défini par rapport au design XML
             vThumbnail = itemView.findViewById(R.id.thumbnail);
             tTitle = itemView.findViewById(R.id.title);
             tCategory = itemView.findViewById(R.id.category);
